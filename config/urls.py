21fc4bd9fd33a525ed2f79c44ce34f5cc9cae272
opencourse2020 <akaaboune@gmail.com>
2026-05-10 21:54:13 +0100

    Initiate the development process

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.conf import settings
 
 urlpatterns = [
-    path("settings.ADMIN_URL", admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
     path("", include("apps.core.urls")),
     path("api/", include("apps.core.api_urls")),
 ]
