ddc480623dbe44f6c57cc8cd9d1154c845ddb4c1
opencourse2020 <akaaboune@gmail.com>
2026-05-10 20:39:30 +0100

    Initiate the development process

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 def main() -> None:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
