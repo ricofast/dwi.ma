7ed0a311266f744b180c3b4128ac2e2e4e8525a2
opencourse2020 <akaaboune@gmail.com>
2026-05-18 21:13:26 +0100

    Initiate the development procecss

--- apps/documents/tasks.py
+++ apps/documents/tasks.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from apps.documents.services.analysis import explain_document
 
 
-@shared_task(bind=True, max_retries=3)
+@shared_task
 def explain_document_task(self, document_id, user_id):
     user = get_user_model().objects.get(id=user_id)
     return str(explain_document(document_id, user).id)
