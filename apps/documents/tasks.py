c8f9745023c7c4d267ebe1aa8b5d91ff25aac04e
opencourse2020 <akaaboune@gmail.com>
2026-05-18 21:16:55 +0100

    Initiate the development procecss

--- apps/documents/tasks.py
+++ apps/documents/tasks.py
@@ -6,6 +6,6 @@ from apps.documents.services.analysis import explain_document
 
 
 @shared_task
-def explain_document_task(self, document_id, user_id):
+def explain_document_task(document_id, user_id):
     user = get_user_model().objects.get(id=user_id)
     return str(explain_document(document_id, user).id)
